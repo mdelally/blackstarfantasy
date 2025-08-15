8af8279477cb267986e1e332cf7f39c28406fa56
Michael DeLally <thoughtreactor@gmail.com>
2025-08-15 17:51:27 -0400

    Trailing slash config; use trailing slash in layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
+  trailingSlash: "always",
   vite: {
     plugins: [tailwindcss()],
   },
